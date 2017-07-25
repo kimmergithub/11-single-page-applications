38344905221205c66088d5bf8c9cb7dcb8afa756
kimmergithub <kimmer@atomicprofits.com>
2017-07-25 10:42:09 -0700

    functions written in our controllers, now we're checkout our work and switching drivers-navs

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,16 +1,20 @@
 'use strict';
 var app = app || {};
 
-// TODO-WORKING: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+// TODONE ??? -WORKING: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 // write the page paths page();
 
 // OUR CONTROLLER === WHEN YOU GO HERE ===> LOAD THIS VIEW
 // THE BACKEND WORK BEHIND CLICKING AN Href LINK (research)
 page('/');
-page('/new');
-page('/admin');
+
+// new.html is where our ARITCLES Render!
+page('/new', articleController.initArticle());
+
+// about information is on our admin page!
+page('/admin', aboutController.initAbout());
 
 
-// TODO-WORKING: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+// TODONE-WORKING: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
 // call page page();
 page();
