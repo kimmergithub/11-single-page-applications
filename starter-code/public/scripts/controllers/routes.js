3f5ddb08012e5ac71d180b7ecd62283be1dd58a1
kimmergithub <kimmer@atomicprofits.com>
2017-07-25 10:25:44 -0700

    added our href and some page directions on our routes

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -4,6 +4,13 @@ var app = app || {};
 // TODO-WORKING: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 // write the page paths page();
 
+// OUR CONTROLLER === WHEN YOU GO HERE ===> LOAD THIS VIEW
+// THE BACKEND WORK BEHIND CLICKING AN Href LINK (research)
+page('/');
+page('/new');
+page('/admin');
+
 
 // TODO-WORKING: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
 // call page page();
+page();
