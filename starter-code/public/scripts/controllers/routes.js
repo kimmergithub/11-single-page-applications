bb698ce47fa5052ed39036a14236759f79830383
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-07-25 12:13:00 -0700

    Christina saved the day! We didn't need to call the function inside of a function on the route thing. LocalHost, we apologize! Come back to ussss!

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -6,11 +6,11 @@ var app = app || {};
 
 // OUR CONTROLLER === WHEN YOU GO HERE ===> LOAD THIS VIEW
 // THE BACKEND WORK BEHIND CLICKING AN Href LINK (research)
-page('/', app.articleController.initArticle());
+page('/', app.articleController.initArticle);
 
 //We added app here because article controller lives on app!! What what!!
 // about information is on our admin page!
-page('/about', app.aboutController.initAbout());
+page('/about', app.aboutController.initAbout);
 
 
 // TODONE-WORKING: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
